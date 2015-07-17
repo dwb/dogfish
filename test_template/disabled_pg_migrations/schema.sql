398d7dd30330a4446a9956992b7c3dc2e03f72c6
Dan Brown <dan@madebymany.co.uk>
2015-07-17 15:09:01 +0100

    Try and make Postgres's schema.sql more backwards-compatible

--- test_template/disabled_pg_migrations/schema.sql
+++ test_template/disabled_pg_migrations/schema.sql
@@ -6,7 +6,6 @@
 --
 
 SET statement_timeout = 0;
-SET lock_timeout = 0;
 SET client_encoding = 'UTF8';
 SET standard_conforming_strings = on;
 SET check_function_bodies = false;
@@ -99,7 +98,6 @@ ALTER TABLE ONLY things
 --
 
 SET statement_timeout = 0;
-SET lock_timeout = 0;
 SET client_encoding = 'UTF8';
 SET standard_conforming_strings = on;
 SET check_function_bodies = false;
